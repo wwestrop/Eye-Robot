ca7449375a113063c184c4b50d3450a885721348
wwestrop <wwestrop@hotmail.com>
2016-07-02 15:55:07 +0100

    Type for "shape" symbols + misnamed file

---
+++ src/Eye-Robot/ShapeEnum.cs
@@ -0,0 +1,24 @@
+﻿namespace EyeRobot {
+
+    internal class ShapeEnum {
+
+        public static readonly ShapeEnum Circle = new ShapeEnum("Circle");
+        public static readonly ShapeEnum Triangle = new ShapeEnum("Triangle");
+        public static readonly ShapeEnum Square = new ShapeEnum("Square");
+        public static readonly ShapeEnum Penatagon = new ShapeEnum("Penatagon");
+        public static readonly ShapeEnum Star4 = new ShapeEnum("Four-pointed star");
+        public static readonly ShapeEnum Star5 = new ShapeEnum("Five-pointed star");
+        public static readonly ShapeEnum Star6 = new ShapeEnum("Six-pointed star");
+
+        private readonly string _name;
+
+        private ShapeEnum(string name) {
+            this._name = name;
+        }
+
+        public override string ToString() {
+            return this._name;
+        }
+    }
+
+}
